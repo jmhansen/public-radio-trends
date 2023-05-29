eaf58cc6f4649c353963ec0038a012d7620463c2
jmh <nesnahnoj@gmail.com>
2023-05-29 11:24:49 -0600

    Switching to npm, etc

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,31 @@
+import { PrismaClient } from '@prisma/client'
 import { Router } from 'express';
 
-
+const prisma = new PrismaClient()
 const router = Router()
 
 router.get('/', (req, res) => {
     return res.json({ message: 'API Home!' })
+})
+
+router.post('/programs', async (req, res) => {
+    try {
+        const { name, url } = req.body
+        const slug = name.toLowerCase().replace(/ /g, '-')
+
+        const newProgram = await prisma.program.create({
+            data: {
+              name: name,
+              slug: slug,
+              url: url
+            },
+          })
+          res.json(newProgram)
+
+    } catch (error: any) {
+        console.log(error.message)
+        res.status(500).json({ error: error.message })
+    }
 })
 
 router.get('/programs', (req, res) => {
